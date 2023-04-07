32a13bf0cf02cdb6f3c240ef89879ee792ee56a5
EmmyMalware <114366661+EmmyMalware@users.noreply.github.com>
2023-04-06 21:42:26 -0500

    code

---
+++ Dextroamphetamine/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Dextroamphetamine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(390, 144);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 1;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 144);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Terrible idea.";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
---
+++ Dextroamphetamine/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Dextroamphetamine
+{
+    public partial class Form1 : Form
+    {
+        private static Random random = new Random();
+        private static int screenW = Screen.PrimaryScreen.Bounds.Width;
+        private static int screenH = Screen.PrimaryScreen.Bounds.Height;
+        private static int ballWidth = 401;
+        private static int ballHeight = 174;
+        private static int ballPosX = random.Next(screenW - ballWidth);
+        private static int ballPosY = random.Next(screenH - ballHeight);
+        private static int moveStepX = 4;
+        private static int moveStepY = 4;
+        public Form1()
+        {
+            InitializeComponent();
+            ballPosX = random.Next(screenW - ballWidth);
+            ballPosY = random.Next(screenH - ballHeight);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private double ti;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int width = this.Width;
+            int height = this.Height;
+            int[] pixelArray = new int[width * height];
+            for (int tt = 0; tt < pixelArray.Length; tt++)
+            {
+                ti += 1;
+                int t = (int)ti;
+                pixelArray[tt] = (int)(
+                    ((t & t / 1 & t / 2) * t / 10E2) % 2
+                    );
+            }
+
+            Bitmap bmp = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+
+            BitmapData bmpData = bmp.LockBits(
+                                new Rectangle(0, 0, bmp.Width, bmp.Height),
+                                ImageLockMode.WriteOnly, bmp.PixelFormat);
+            Marshal.Copy(pixelArray, 0, bmpData.Scan0, pixelArray.Length);
+
+            bmp.UnlockBits(bmpData);
+            pictureBox1.Image = bmp;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            this.Size = new Size(300, 150);
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            this.Location = new Point(ballPosX, ballPosY);
+            ballPosX += moveStepX;
+            if (
+                ballPosX < 0 ||
+                ballPosX + ballWidth > screenW
+                )
+            {
+                moveStepX = -moveStepX;
+            }
+
+            ballPosY += moveStepY;
+            if (
+                ballPosY < 0 ||
+                ballPosY + ballHeight > screenH
+                )
+            {
+                moveStepY = -moveStepY;
+            }
+        }
+    }
+}
